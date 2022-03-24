f34c882424ab23bb9ca43b98d6abef2c0296665c
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-03-25 02:59:47 +0500

    Button hover effects

--- components/navigation/Button/Button.tsx
+++ components/navigation/Button/Button.tsx
@@ -11,7 +11,7 @@ const Button: NextPage<Props> = ({ to, secondary, light, children }) => {
   const base = 'font-bold inline-block';
   const conditional = secondary
     ? 'hover:underline rounded-md px-1'
-    : 'border-2 rounded-full py-2 px-4';
+    : 'border-2 rounded-full py-2 px-4 hover:bg-white hover:text-sky-700 transition-colors';
   const color = light
     ? 'text-white border-white'
     : 'text-sky-700 border-sky-700';
