eff8b47f1ace13739d100403606ca4ce4c2c8eea
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-03-24 23:33:40 +0500

    Added three more testimonials and simple and extended version

--- components/Testimonials/Testimonials.tsx
+++ components/Testimonials/Testimonials.tsx
@@ -1,7 +1,11 @@
 import { NextPage } from 'next';
 import TestimonialItem from './TestimonialItem/TestimonialItem';
 
-const Testimonaials: NextPage = () => {
+interface Props {
+  simple?: boolean;
+}
+
+const Testimonaials: NextPage<Props> = ({ simple }) => {
   return (
     <section className="container mx-auto px-2 my-20 space-y-12">
       <div className="space-y-6">
@@ -15,17 +19,39 @@ const Testimonaials: NextPage = () => {
           stars={5}
         />
         <TestimonialItem
-          name="John Doe"
+          name="Mitch Goodman"
           quote="Every great dream begins with a dreamer. Always remember, you have within you the strength, the patience, and the passion to reach for the stars to change the world."
           image="2.jpg"
           stars={5}
         />
         <TestimonialItem
-          name="John Doe"
+          name="Sarah Harden"
           quote="When I dare to be powerful – to use my strength in the service of my vision, then it becomes less and less important whether I am afraid."
           image="3.jpg"
           stars={5}
         />
+        {!simple && (
+          <>
+            <TestimonialItem
+              name="Matthew Jones"
+              quote="Your time is limited, so don't waste it living someone else's life. Don't be trapped by dogma – which is living with the results of other people's thinking."
+              image="4.jpg"
+              stars={5}
+            />
+            <TestimonialItem
+              name="Steve Jobs"
+              quote="You have brains in your head. You have feet in your shoes. You can steer yourself any direction you choose."
+              image="5.jpg"
+              stars={4}
+            />
+            <TestimonialItem
+              name="Megan Marley"
+              quote="The real test is not whether you avoid this failure, because you won't. It's whether you let it harden or shame you into inaction, or whether you learn from it; whether you choose to persevere."
+              image="6.jpg"
+              stars={5}
+            />
+          </>
+        )}
       </div>
     </section>
   );
