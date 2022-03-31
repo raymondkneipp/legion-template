b56be8d017570bcef60b8b6ef1ac672011f5c79d
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-04-01 00:41:05 +0500

    Footer layout change

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from 'next';
-import Link from 'next/link';
+import Image from 'next/image';
 import {
   FaInstagram,
   FaFacebook,
@@ -8,41 +8,85 @@ import {
   FaTiktok,
   FaRegCopyright,
 } from 'react-icons/fa';
-import { Container, Typography } from '..';
+import {
+  Container,
+  DescriptionList,
+  DescriptionListItem,
+  Tall,
+  Typography,
+} from '..';
 import PageLink from './PageLink/PageLink';
-import SocialIcon from './SocialIcon/SocialIcon';
 
 const Footer: NextPage = () => {
   return (
     <footer className="my-10">
       <Container>
-        <div className="flex flex-col space-y-6 items-center justify-between">
-          <div className="flex flex-col items-center space-y-2 justify-center sm:flex-row sm:space-y-0 sm:space-x-4">
-            <PageLink to="/" page="Home" />
-            <PageLink to="/about" page="About" />
-            <PageLink to="/services" page="Services" />
-            <PageLink to="/contact" page="Contact" />
-            <PageLink to="/privacy-policy" page="Privacy Policy" />
-            <PageLink to="/terms-of-use" page="Terms of Use" />
-            <PageLink to="/not-found" page="404" />
-          </div>
+        <Tall
+          master={
+            <>
+              <Image src="/logo.svg" width={45} height={45} layout="fixed" />
+              <Typography variant="p">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima,
+                optio? Odio repudiandae ipsa ipsam non id iusto, incidunt ullam
+              </Typography>
+            </>
+          }
+          slave={
+            <div className="flex flex-wrap justify-around gap-6 col-span-2">
+              <DescriptionList header="PAGES">
+                <DescriptionListItem>
+                  <PageLink to="/" page="Home" />
+                </DescriptionListItem>
+                <DescriptionListItem>
+                  <PageLink to="/about" page="About" />
+                </DescriptionListItem>
+                <DescriptionListItem>
+                  <PageLink to="/services" page="Services" />
+                </DescriptionListItem>
+                <DescriptionListItem>
+                  <PageLink to="/contact" page="Contact" />
+                </DescriptionListItem>
+              </DescriptionList>
+              <DescriptionList header="LEGAL">
+                <DescriptionListItem>
+                  <PageLink to="/privacy-policy" page="Privacy Policy" />
+                </DescriptionListItem>
+                <DescriptionListItem>
+                  <PageLink to="/terms-of-use" page="Terms of Use" />
+                </DescriptionListItem>
+                <DescriptionListItem>
+                  <PageLink to="/not-found" page="404" />
+                </DescriptionListItem>
+              </DescriptionList>
+              <DescriptionList header="SOCIAL">
+                <DescriptionListItem icon={FaFacebook}>
+                  <PageLink to="https://www.facebook.com/" page="Facebook" />
+                </DescriptionListItem>
+                <DescriptionListItem icon={FaInstagram}>
+                  <PageLink
+                    to="https://www.instagram.com/?hl=en"
+                    page="Instagram"
+                  />
+                </DescriptionListItem>
+                <DescriptionListItem icon={FaTwitter}>
+                  <PageLink to="https://twitter.com/?lang=en" page="Twitter" />
+                </DescriptionListItem>
+                <DescriptionListItem icon={FaLinkedin}>
+                  <PageLink to="https://www.linkedin.com/" page="LinkedIn" />
+                </DescriptionListItem>
+                <DescriptionListItem icon={FaTiktok}>
+                  <PageLink to="https://www.tiktok.com/en/" page="TikTok" />
+                </DescriptionListItem>
+              </DescriptionList>
+            </div>
+          }
+        />
 
-          <div className="flex items-center space-x-2 sm:space-x-6">
-            <SocialIcon
-              to="https://www.instagram.com/?hl=en"
-              icon={FaInstagram}
-            />
-            <SocialIcon to="https://www.facebook.com/" icon={FaFacebook} />
-            <SocialIcon to="https://twitter.com/?lang=en" icon={FaTwitter} />
-            <SocialIcon to="https://www.linkedin.com/" icon={FaLinkedin} />
-            <SocialIcon to="https://www.tiktok.com/en/" icon={FaTiktok} />
-          </div>
-          <div className="space-x-1 flex items-center text-gray-600">
-            <FaRegCopyright />
-            <Typography variant="p">
-              2022 Brand, Inc. All rights reserved.
-            </Typography>
-          </div>
+        <div className="space-x-1 flex items-center justify-center text-gray-600 my-10">
+          <FaRegCopyright />
+          <Typography variant="p">
+            2022 Brand, Inc. All rights reserved.
+          </Typography>
         </div>
       </Container>
     </footer>
