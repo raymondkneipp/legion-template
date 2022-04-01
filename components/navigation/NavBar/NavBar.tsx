e4f3d13ed6b85c966c101810a7376e1f1986714c
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-04-01 16:38:06 +0500

    Navbar now is responsive

--- components/navigation/NavBar/NavBar.tsx
+++ components/navigation/NavBar/NavBar.tsx
@@ -1,12 +1,16 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 import { Container, Typography } from '../..';
 import ActiveLink from './ActiveLink/ActiveLink';
+import MenuButton from './MenuButton/MenuButton';
 
 const NavBar: NextPage = () => {
+  const [show, setShow] = useState(false);
+
   return (
-    <nav className="border-b border-gray-300 py-4">
+    <nav className="bg-white border-b border-gray-300 py-4 sticky top-0 left-0 right-0 z-50">
       <Container>
         <div className="flex items-center justify-between">
           <Link href="/">
@@ -18,12 +22,20 @@ const NavBar: NextPage = () => {
             </a>
           </Link>
 
-          <div className="md:flex items-center space-x-4 hidden">
-            <ActiveLink to="/">Home</ActiveLink>
-            <ActiveLink to="/about">About</ActiveLink>
-            <ActiveLink to="/services">Services</ActiveLink>
-            <ActiveLink to="/contact">Contact</ActiveLink>
+          <div
+            className={`md:block ${
+              show ? 'block' : 'hidden'
+            } absolute top-full bg-white left-0 right-0 p-2 pt-0 border-b border-gray-300 md:relative md:border-0 md:p-0`}
+          >
+            <div className="flex flex-col space-y-2 md:space-y-0 md:flex-row md:flex md:items-center md:space-x-4">
+              <ActiveLink to="/">Home</ActiveLink>
+              <ActiveLink to="/about">About</ActiveLink>
+              <ActiveLink to="/services">Services</ActiveLink>
+              <ActiveLink to="/contact">Contact</ActiveLink>
+            </div>
           </div>
+
+          <MenuButton show={show} setShow={setShow} />
         </div>
       </Container>
     </nav>
