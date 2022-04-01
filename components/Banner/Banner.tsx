fb373b863da7fb8f39fde70480b233b1010cfd0d
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-04-01 17:08:05 +0500

    Banner close button removed

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -1,17 +1,13 @@
 import { NextPage } from 'next';
 import React from 'react';
-import { FaTimes } from 'react-icons/fa';
 import { Container } from '..';
 
 const Banner: NextPage = ({ children }) => {
   return (
     <aside className="bg-sky-700 text-center py-3">
       <Container>
-        <div className="flex items-center justify-between space-x-4">
-          <div className="flex items-center justify-center space-x-4 flex-1">
-            {children}
-          </div>
-          <FaTimes color="white" />
+        <div className="flex items-center justify-center space-x-4">
+          {children}
         </div>
       </Container>
     </aside>
