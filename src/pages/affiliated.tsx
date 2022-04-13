a1ea497ac09f2e1d2ee0a7af237dda0a9b3b6f4e
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-04-13 19:43:35 -0400

    Added links

--- src/pages/affiliated.tsx
+++ src/pages/affiliated.tsx
@@ -31,18 +31,17 @@ const AffiliatedPage: NextPage = () => {
 							</>
 						}
 						slave={
-							<h1>hello</h1>
-							// <>
-							// 	{affiliated.lists.map((list: any, i: number) => (
-							// 		<DescriptionList header={list.title} key={i}>
-							// 			<DescriptionListItem>
-							// 				{/* {list.links.map((item: any, j: number) => (
-							// 					<PageLink to={item.link} page={item.title} key={j} />
-							// 				))} */}
-							// 			</DescriptionListItem>
-							// 		</DescriptionList>
-							// 	))}
-							// </>
+							<>
+								{affiliated.lists.map((list: any, i: number) => (
+									<DescriptionList header={list.title} key={i}>
+										<DescriptionListItem>
+											{list.links.map((item: any, j: number) => (
+												<PageLink to={item.link} page={item.title} key={j} />
+											))}
+										</DescriptionListItem>
+									</DescriptionList>
+								))}
+							</>
 						}
 					/>
 				</Container>
