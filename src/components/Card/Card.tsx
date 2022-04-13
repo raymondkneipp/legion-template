047f8ed7a1c3a901ed05c4a21fc95cccbb72e4fe
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-04-12 22:52:33 -0400

    Cards have max height for long text

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -16,9 +16,11 @@ const Card: NextPage<Props> = ({ image, date, title, content }) => (
 		</div>
 		<div className="p-6 space-y-6 flex flex-col flex-1">
 			<Typography variant="h3">{title}</Typography>
-			<div className="flex-1" dangerouslySetInnerHTML={{ __html: content }} />
-			{/* <Typography variant="p">{content}</Typography> */}
-			{/* </div> */}
+			<div
+				className="flex-1 max-h-36 overflow-hidden"
+				dangerouslySetInnerHTML={{ __html: content }}
+			/>
+
 			<Typography variant="p" color="primary">
 				{date}
 			</Typography>
