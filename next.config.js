844abf3d3cfc5ccd43b7154281e6b22c9089913a
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-05-16 17:12:19 -0400

    Removed markdown stuff

--- next.config.js
+++ next.config.js
@@ -2,15 +2,6 @@
 
 const nextConfig = {
 	reactStrictMode: true,
-	webpack: (cfg) => {
-		cfg.module.rules.push({
-			test: /\.md$/,
-			loader: "frontmatter-markdown-loader",
-			// options: { mode: ["react-component"] },
-		});
-
-		return cfg;
-	},
 	i18n: {
 		locales: ["en"],
 		defaultLocale: "en",
