2449beb835dfa204953fdc1d9fc4c5bf64f3b8be
Raymond Kneipp <raymond.f.kneipp@gmail.com>
2022-05-28 16:20:06 -0400

    Protected route

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,23 +1,19 @@
 import type { NextPage } from "next";
-import { NextSeo } from "next-seo";
-import {
-	Container,
-	DashboardNav,
-	Input,
-	Submit,
-	ColoredLink,
-	DashboardLayout,
-} from "@components";
+import { Input, DashboardLayout } from "@components";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { useAppDispatch, useAppSelector } from "@store";
 import { updatePostData } from "@store/post/post.actions";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 interface IGeneralValues {
 	Name: string;
 }
 
 const Dashboard: NextPage = () => {
+	const router = useRouter();
 	const { name } = useAppSelector((state) => state.post);
+	const { authenticated, loading } = useAppSelector((state) => state.auth);
 	const dispatch = useAppDispatch();
 	const { register, handleSubmit } = useForm<IGeneralValues>({
 		defaultValues: { Name: name },
@@ -25,18 +21,23 @@ const Dashboard: NextPage = () => {
 
 	const onSubmit: SubmitHandler<IGeneralValues> = (data) => {
 		dispatch(updatePostData(data.Name));
-
-		alert(JSON.stringify(data));
 	};
 
-	return (
-		<>
-			<NextSeo title="Dashboard" />
+	useEffect(() => {
+		if (!authenticated && !loading) {
+			router.push("/login");
+		}
+	}, [authenticated, loading]);
+
+	if (authenticated) {
+		return (
 			<DashboardLayout onSubmit={handleSubmit(onSubmit)}>
 				<Input label="Name" register={register} />
 			</DashboardLayout>
-		</>
-	);
+		);
+	}
+
+	return null;
 };
 
 export default Dashboard;
